52220f371fc9e962695192c0b067609069ed7c6e
An00byss <17164642+an00byss@users.noreply.github.com>
2020-09-21 09:29:00 -0400

    Updated tool and readme
    Updated tool and readme

--- thecl0n3r.py
+++ thecl0n3r.py
@@ -1,5 +1,3 @@
-# by an00byss
-# Made for pentesters and red teamers.
 
 import argparse
 import subprocess
@@ -10,8 +8,8 @@ import datetime
 import requests
 from requests.exceptions import HTTPError
 
-"""User Github API Key"""
-api_key = "token " + "ADD-YOUR-GITHUB-API-KEY" # Replace API Token
+"""User Github API Token"""
+api_token = "ADD-GITHUB-TOKEN-HERE" # Replace API Token
 
 def prRed(skk): print("\033[91m {}\033[00m" .format(skk))
 def prYellow(skk): print("\033[93m {}\033[00m" .format(skk))
@@ -19,23 +17,27 @@ def prGreen(skk): print("\033[92m {}\033[00m" .format(skk))
 def prPurple(skk): print("\033[95m {}\033[00m" .format(skk))
 def prCyan(skk): print("\033[96m {}\033[00m" .format(skk))
 
-parser = argparse.ArgumentParser(description=prGreen("[*] TheCl0n3r will allow you to download and manage your git repositories.\n"))
+parser = argparse.ArgumentParser(description=prGreen("[*] TheCl0n3r will allow you to download and manage your git repositories."))
+parser = argparse.ArgumentParser(description=prGreen("[*] Made for pentesters and red teamers. By @An00byss\n"))
 parser.add_argument('-l', '--list', help='List all currently installed tools', action='store_true')
 parser.add_argument('-d', '--download', help='Download git tools', action='store_true')
 parser.add_argument('-a', '--add', metavar='Link', help='Add git tool, Provide a github link.', type=str)
 parser.add_argument('-u', '--update', help='Update git tool, Specify "TOOL" or "all".', type=str)
 parser.add_argument('-r', '--remove', metavar='Tool', help='Deletes git tools. "ALL" for all tools or "TOOL" to delete specified tool.', type=str)
+parser.add_argument('-s', '--search', metavar='Search', help='Search for tools on github', type=str)
+parser.add_argument('-i', '--install', help='Will attempt to auto build & Setup Go and Python tools. "Both", "GO" or "Python"', type=str)
 
 """ File containing all of our tools"""
 filename = "tool_list.txt"
 
-"""Sets path of our tool"""
+""" Sets path of our tool"""
 ourpath = os.path.dirname(os.path.abspath(__file__))
 
+"""  Set git headers """
+git_headers = {"User-Agent": "curl/7.68.0", "Accept": "*/*", "Authorization": "token " + api_token, "Connection": "close", "Accept-Encoding": "gzip, deflate"}
 
 def list_tools():
     """Listing currently tools"""
-    git_headers = {"User-Agent": "curl/7.68.0", "Accept": "*/*", "Authorization": api_key, "Connection": "close", "Accept-Encoding": "gzip, deflate"}
     
     try:
         list_num = 1
@@ -82,7 +84,7 @@ def download_tools():
         prCyan("[*] Downloading tools... Please wait.")
         with open(filename) as file_object:
             for line in file_object:
-                subprocess.run(["/usr/bin/git", "clone", line.strip()], shell=False, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, encoding="utf-8")
+                subprocess.run(["/usr/bin/git", "clone", line.strip()], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
         prYellow("[*] All tools downloaded.")
 
     except FileNotFoundError:
@@ -91,19 +93,28 @@ def download_tools():
 
 
 def remove_tools(deleted_tools):
-    """Will delete specified tool in list."""        
+    """Will delete specified tool in list."""
+    pipenv = os.environ['HOME'] + "/.local/bin/pipenv"
+    
     if deleted_tools == "ALL":
         prCyan("[*] Making a backup of tool list.")
         shutil.copy(filename, filename + ".bak")
         
+        #with open(filename) as file_object:
+        #    # Delete's all pipenv installations.
+        #    for tool in file_object:
+        #        os.chdir(ourpath + "/" + tool.split("/")[4].strip())
+        #        subprocess.run([pipenv, '--rm'], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+
         with open(filename) as file_object:
             for line in file_object:
                 tool_split = line.split('/')
                 delete = tool_split[4]
+                os.chdir(ourpath + "/" + delete.strip())
+                subprocess.run([pipenv, '--rm'], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
                 subprocess.Popen(["/usr/bin/rm", "-rf", ourpath + "/" + delete.strip()])
-        
-        with open(filename, 'w') as file_object:
-            file_object.write("")
+
+        os.remove(ourpath + "/" + filename)
         prYellow("[*] All Tools have been deleted!")
 
     elif deleted_tools == deleted_tools:
@@ -124,6 +135,8 @@ def remove_tools(deleted_tools):
                                 delete = tool_split[4]
                                 if deleted_tools.strip() not in line.strip():
                                     file_object.write(line)
+                                    os.chdir(ourpath + "/" + deleted_tools.strip())
+                                    subprocess.run([pipenv, '--rm'], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
                                     subprocess.Popen(["/usr/bin/rm", "-rf", ourpath + "/" + deleted_tools.strip()])
                     prYellow("[*] " + deleted_tools + " has been removed.")
                     quit()
@@ -138,10 +151,7 @@ def remove_tools(deleted_tools):
 
 def update_tools(update_choice):
     """Will update all installed it tools"""
-    
-    git_headers = {"User-Agent": "curl/7.68.0", "Accept": "*/*", "Authorization": api_key, "Connection": "close", "Accept-Encoding": "gzip, deflate"}
 
-    
     type_update = input(" [*] Do you want to 'pull' or 'fresh' update: ")
     
     if update_choice != None:
@@ -168,7 +178,7 @@ def update_tools(update_choice):
                             subprocess.run(["/usr/bin/rm", "-rf", ourpath + "/" + delete.strip()])
                             with open(filename) as file_object:
                                 for line in file_object:
-                                    subprocess.run(["/usr/bin/git", "clone", line.strip()], shell=False, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, encoding="utf-8")
+                                    subprocess.run(["/usr/bin/git", "clone", line.strip()], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
                                 prCyan("[*] " + tool_split[4].strip() + " has been updated.\n")
                             
                         else:
@@ -180,7 +190,7 @@ def update_tools(update_choice):
                         delete = tool_split[4]
                         with open(filename) as file_object:
                             for line in file_object:
-                                subprocess.run(["/usr/bin/git", "pull", line.strip()], shell=False, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, encoding="utf-8")
+                                subprocess.run(["/usr/bin/git", "pull", line.strip()], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
                             prCyan("[*] " + tool_split[4].strip() + " has been updated.\n")
                     else:
                         prRed("Not a recognized update type.")
@@ -198,7 +208,7 @@ def update_tools(update_choice):
                             subprocess.run(["/usr/bin/rm", "-rf", ourpath + "/" + delete.strip()])
                             with open(filename) as file_object:
                                 for line in file_object:
-                                    subprocess.run(["/usr/bin/git", "clone", line.strip()], shell=False, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, encoding="utf-8")
+                                    subprocess.run(["/usr/bin/git", "clone", line.strip()], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
                                 prCyan("[*] " + tool_split[4].strip() + " has been updated.\n")
                                 quit()
                         else:
@@ -209,18 +219,117 @@ def update_tools(update_choice):
                         delete = update_choice.strip() # change the tool we are deleting to our choice
                         with open(filename) as file_object:
                             for line in file_object:
-                                subprocess.run(["/usr/bin/git", "pull", line.strip()], shell=False, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, encoding="utf-8")
+                                subprocess.run(["/usr/bin/git", "pull", line.strip()], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
                             prCyan("[*] " + tool_split[4].strip() + " has been updated.\n")
                     else:
                         prRed("Not a recognized update type.")
-                
-                else:
-                    prPurple("[*] Not the specified tool tool, skipping...")
                     
                     
     else:
         prRed("[*] Please choose 'All' or 'Tool'.")
 
+def search_tools(stool):
+    """Searches github for tools"""
+
+    if stool != None:
+        try:
+            response = requests.get("https://api.github.com/search/repositories?q=" + stool, headers=git_headers)
+            r_dict = response.json()
+
+            results = []
+            for key,value in r_dict.items():
+                if key == "items":
+                    results.append(value)
+            
+            contents = results[0]
+            counter = 1
+            for line in contents:
+                for key, value in line.items():
+                    if key == "full_name":
+                        prYellow("[" + str(counter) + "]" + " Creator: " + value)
+                    elif key == "html_url":
+                        print(" [+]" + " " + str(value))
+                    elif key == "description":
+                        if value == None:
+                            prRed("[*] There is no description for this tool.\n")
+                        else:
+                            prCyan("[*] Description: " + str(value) + "\n")
+                counter += 1
+            prGreen("\n [*] Best match results for: " + stool.title())
+            
+        except ConnectionError:
+            print("Could not connect, check connection.")
+    
+    else:
+        print("Please enter in a tool to search.")
+
+def install_tools(choosebld):
+    """ Will attempt to build & setup go and python tools """
+    pipenv = os.environ['HOME'] + "/.local/bin/pipenv"
+    goext = ".go"
+    
+    if choosebld == "both" or choosebld == "BOTH":
+        prPurple("[*] Building Go binaries 'Linux only' and VirtualEnv's via pipenv for Python installations.")
+
+        with open(filename) as file_object:
+            for tool in file_object:
+                with os.scandir(ourpath + "/" + tool.split("/")[4].strip()) as dirs:
+                    bld = tool.split("/")[4].strip()
+                    for entry in dirs:
+                        if entry.name == "main.go" or entry.name == bld + goext: # If a main.go is found it will build a go binary.
+                            prYellow("[+] Found a GO installation... Building: " + tool.split("/")[4].strip())
+                            os.chdir(ourpath + "/" + tool.split("/")[4].strip())
+                            subprocess.run(['/usr/local/go/bin/go', 'build', '-o', tool.split("/")[4].strip()], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+                        elif entry.name == "requirements.txt":
+                            try:
+                                prYellow("[+] Found a Python installation... Building: " + tool.split("/")[4].strip())
+                                os.chdir(ourpath + "/" + tool.split("/")[4].strip())
+                                subprocess.run([pipenv, 'install'], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+                            except FileNotFoundError:
+                                prRed("[*] Pipenv not installed or not in '~/.local/bin' directory.")
+                                continue
+    
+    elif choosebld == "go" or choosebld == "GO":
+        goext = ".go"
+        prPurple("[*] Building Go binaries 'Linux only'")
+        
+        """ Will only build go programs """
+        with open(filename) as file_object:
+            for tool in file_object:
+                bld = tool.split("/")[4].strip()
+                for file in os.listdir(ourpath + "/" + bld):
+                    if file == "main.go" or file == bld + goext:
+                        prYellow("[+] Found a GO installation... Building: " + bld)
+                        os.chdir(ourpath + "/" + bld)
+                        try:
+                            subprocess.run(['/usr/local/go/bin/go', 'build', '-o', tool.split("/")[4].strip()], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+                        except:
+                            print("[*] Something went wrong, will need to manually build.")
+                            continue
+
+
+    elif choosebld == "python" or choosebld == "PYTHON":
+        """ Will only build if python is chosen"""
+        prCyan("[*] Installing all found python programs.")
+        prRed("[*] Caution! Python virtualenvs can get large.\n")
+
+        with open(filename) as file_object:
+            for tool in file_object:
+                with os.scandir(ourpath + "/" + tool.split("/")[4].strip()) as dirs:
+                    for entry in dirs:
+                        if entry.name == "requirements.txt":
+                            try:
+                                prYellow("[+] Found a Python installation... Building: " + tool.split("/")[4].strip())
+                                os.chdir(ourpath + "/" + tool.split("/")[4].strip())
+                                subprocess.run([pipenv, 'install'], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+                            except FileNotFoundError:
+                                prRed("[*] Pipenv not installed or not in '~/.local/bin' directory.")
+                                continue
+    
+    else:
+        prRed("[*] Please choose 'Both', 'GO' or 'Python'.")
+
+    prCyan("[*] All Done! You can thank me later. ( ͡~ ͜ʖ ͡°)")
 
 
 def main():
@@ -244,6 +353,14 @@ def main():
     elif args.update:
         update_choice = str(args.update).strip()
         update_tools(update_choice)
+    
+    elif args.search:
+        stool = str(args.search).strip()
+        search_tools(stool)
+    
+    elif args.install:
+        choosebld= str(args.install).strip()
+        install_tools(choosebld)
 
 
 if __name__ == "__main__":
